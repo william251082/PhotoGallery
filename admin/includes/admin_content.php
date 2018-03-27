09845cc513e66e73113729f3cd240ffa7012d886
William del Rosario <williamdelrosario@yahoo.com>
2018-03-27 14:03:05 +0200

    Assigning array values to object properties.

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,14 +9,23 @@
         </h1>
 
         <?php
-            $result_set = User::find_all_users();
-
-            while ($row = mysqli_fetch_array($result_set)) {
-                echo $row['username'] . "<br>";
-            }
+//            $result_set = User::find_all_users();
 //
-//            $found_user = User::find_user_by_id(1);
-//            echo $found_user['username']
+//            while ($row = mysqli_fetch_array($result_set)) {
+//                echo $row['username'] . "<br>";
+//            }
+
+            $found_user = User::find_user_by_id(1);
+
+            $user = new User();
+
+            $user->id = $found_user['id'];
+            $user->username = $found_user['username'];
+            $user->password = $found_user['password'];
+            $user->first_name = $found_user['first_name'];
+            $user->last_name = $found_user['last_name'];
+
+            echo $user->id;
         ?>
 
         <ol class="breadcrumb">
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -8,6 +8,13 @@
 
 class User
 {
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
+
+
     public static function find_all_users()
     {
         return self::find_this_query("SELECT * FROM users");
