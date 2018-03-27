a4fe9fbc4709728330b0f4b6c934bcca54890be7
William del Rosario <williamdelrosario@yahoo.com>
2018-03-27 12:15:13 +0200

    Improved db connection to be more oop

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -12,7 +12,7 @@
             $sql = "SELECT * FROM users WHERE id=1";
             $result = $database->query($sql);
             $user_found = mysqli_fetch_array($result);
-            
+
             echo $user_found['username'];
         ?>
 
