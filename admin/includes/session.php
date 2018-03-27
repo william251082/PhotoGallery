299b9964b3b7fe9ac7e7a83fe2173bbb73da7e68
William del Rosario <williamdelrosario@yahoo.com>
2018-03-27 16:03:24 +0200

    Checking login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -15,6 +15,17 @@ class Sessions
     {
         session_start();
     }
+
+    private function check_the_login()
+    {
+        if(isset($_SESSION['user_id'])) {
+            $this->user_id = $_SESSION['user_id'];
+            $this->signed_in = true;
+        } else {
+            unset($this->user_id);
+            $this->signed_in = false;
+        }
+    }
 }
 
 $session = new Sessions();
