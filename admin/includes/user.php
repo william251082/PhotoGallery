9998b94f3239726fab9ed5ef1d1aad8bf06d3df3
William del Rosario <williamdelrosario@yahoo.com>
2018-03-27 20:59:26 +0200

    Created the create method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -91,4 +91,16 @@ class User
 
         return array_key_exists($the_attribute, $object_properties);
     }
+
+    public function create()
+    {
+        global $database;
+
+        $sql = "INSERT INTO users (username, password, first_name, last_name)";
+        $sql .= "VALUES('";
+        $sql .= $database->escape_string($this->username) . "', '";
+        $sql .= $database->escape_string($this->password) . "', '";
+        $sql .= $database->escape_string($this->first_name) . "', '";
+        $sql .= $database->escape_string($this->last_name) . "')";
+    }
 }
