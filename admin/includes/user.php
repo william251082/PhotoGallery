def03441ab0a71607352ddcec9866f25279559b8
William del Rosario <williamdelrosario@yahoo.com>
2018-03-27 14:17:19 +0200

    Aut-instatiation, short.

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -36,15 +36,21 @@ class User
         return $result_set;
     }
 
-    public static function instantiation($found_user)
+    public static function instantiation($the_record)
     {
         $the_object = new self;
 
-        $the_object->id         = $found_user['id'];
-        $the_object->username   = $found_user['username'];
-        $the_object->password   = $found_user['password'];
-        $the_object->first_name = $found_user['first_name'];
-        $the_object->last_name  = $found_user['last_name'];
+//        $the_object->id         = $found_user['id'];
+//        $the_object->username   = $found_user['username'];
+//        $the_object->password   = $found_user['password'];
+//        $the_object->first_name = $found_user['first_name'];
+//        $the_object->last_name  = $found_user['last_name'];
+        
+        foreach ($the_record as $the_attribute => $value){
+            if ($the_object->has_the_attribute($the_attribute)){
+                $the_object->the_attribute = $value;
+            }
+        }
 
         return $the_object;
     }
