65d68febfcf7ef0984555ece2dca3bfb27643c11
William del Rosario <williamdelrosario@yahoo.com>
2018-03-28 13:14:59 +0200

    Delete page p2

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -1,8 +1,23 @@
 <?php include("includes/init.php"); ?>
+
 <?php
 if (!$session->is_signed_in()){
 
     redirect("login.php");
 }
-echo "it works!";
+?>
+
+<?php
+
+if (empty($GET['photo_id'])) {
+    redirect("photos.php");
+}
+
+$photo = Photo::find_user_by_id($_GET['photo_id']);
+
+if ($photo) {
+    $photo->delete_photo();
+} else {
+    redirect("photos.php");
+}
 ?>
