bbcf3d18ab5e3d6406b55056f7ab371b131efb0a
William del Rosario <williamdelrosario@yahoo.com>
2018-03-28 09:23:11 +0200

    Building directory paths

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,5 +1,13 @@
 <?php
 
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+define('SITE_ROOT', DS . 'Applications' . DS . 'MAMP' . DS . 'htdocs' . DS . 'photogallery' );
+
+defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT.DS.'admin'.DS.'includes');
+
+defined('IMAGES_PATH') ? null : define('IMAGES_PATH', SITE_ROOT.DS.'admin'.DS.'images');
+
 require_once ("functions.php");
 require_once("new_config.php");
 require_once ("database.php");
