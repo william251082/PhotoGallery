162d1e6f7d6dde0a77e0a80454a2c3f649a97824
William del Rosario <williamdelrosario@yahoo.com>
2018-03-28 13:06:05 +0200

    Delete button

--- admin/photos.php
+++ admin/photos.php
@@ -50,7 +50,15 @@ $photos = Photo::find_all();
                             <tbody>
                             <?php foreach ($photos as $photo) : ?>
                             <tr>
-                                <td><img src="<?php echo $photo->picture_path(); ?>" alt=""></td>
+                                <td><img src="<?php echo $photo->picture_path(); ?>" alt="">
+
+                                    <div class="pictures_link">
+                                        <a href="delete_photo.php/?id=<?php echo $photo->photo_id; ?>">Delete</a>
+                                        <a href="#">Edit</a>
+                                        <a href="#">View</a>
+                                    </div>
+
+                                </td>
                                 <td><?php echo $photo->photo_id; ?></td>
                                 <td><?php echo $photo->filename; ?></td>
                                 <td><?php echo $photo->title; ?></td>
