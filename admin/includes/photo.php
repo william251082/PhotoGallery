826c12b3621c0363c27cad891d74925c976e3118
William del Rosario <williamdelrosario@yahoo.com>
2018-03-28 09:00:28 +0200

    Added class properties

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,12 +1,27 @@
 <?php
-///**
-// * Created by PhpStorm.
-// * User: williamdelrosario
-// * Date: 3/28/18
-// * Time: 8:40 AM
-// */
-//
-//class Photo
-//{
-//
-//}
+/**
+ * Created by PhpStorm.
+ * User: williamdelrosario
+ * Date: 3/28/18
+ * Time: 8:40 AM
+ */
+
+class Photo extends Db_object
+{
+    protected static $db_table = "photos";
+    protected static $db_table_fields = array(
+        'photo_id',
+        'title',
+        'description',
+        'filename',
+        'type',
+        'size'
+    );
+
+    public $Photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
+}
